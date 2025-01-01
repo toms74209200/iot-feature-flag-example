a42fcf284b0ec1f00e0f47edb2e48cfa4b956311
toms74209200 <motomotomato@gmail.com>
2025-01-01 16:11:51 +0900

    :sparkles: Add MQTT frontend client

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,118 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { connectMqtt, publishMqtt, subscribeMqtt } from "./model/mqtt";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [connected, setConnected] = useState<boolean>(false);
+  const [subscribed, setSubscribed] = useState<boolean>(false);
+  const [subscribeTopic, setSubscribeTopic] = useState<string>("");
+  const [sendMessage, setSendMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className={["w-full", "mt-12"].join(" ")}>
+      <div
+        className={[
+          "m-auto",
+          "max-w-3xl",
+          "flex",
+          "flex-col",
+          "space-y-8",
+        ].join(" ")}
+      >
+        <div
+          className={["flex", "flex-row", "justify-center", "space-x-2"].join(
+            " "
+          )}
+        >
+          <button
+            className={[
+              "bg-blue-600",
+              "text-white",
+              "font-bold",
+              "rounded-md",
+              "px-3",
+              "active:bg-blue-700/90",
+              "disabled:opacity-25",
+            ].join(" ")}
+            onClick={() => {
+              connectMqtt(() => {});
+              setConnected(true);
+            }}
+            disabled={connected}
+          >
+            {connected ? "Connected" : "Connect"}
+          </button>
+          <input
+            type="text"
+            className={["border", "rounded-md", "py-1", "px-2"].join(" ")}
+            placeholder="Subscribe Topic"
+            onChange={(e) => {
+              setSubscribeTopic(e.target.value);
+            }}
+            disabled={!connected || subscribed}
+          />
+          <button
+            className={[
+              "bg-amber-600",
+              "text-white",
+              "font-bold",
+              "rounded-md",
+              "px-3",
+              "active:bg-amber-700/90",
+              "disabled:opacity-25",
+            ].join(" ")}
+            onClick={() => {
+              subscribeMqtt(subscribeTopic, (_topic, message) => {
+                setMessages((prev) => [...prev, message]);
+              });
+              setSubscribed(true);
+            }}
+            disabled={!connected || subscribed || subscribeTopic === ""}
+          >
+            {subscribed ? "Subscribed" : "Subscribe"}
+          </button>
+        </div>
+        <div
+          className={["flex", "flex-row", "justify-center", "space-x-2"].join(
+            " "
+          )}
+        >
+          <input
+            type="text"
+            className={["border", "rounded-md", "py-1", "px-2"].join(" ")}
+            placeholder="Send Message"
+            value={sendMessage}
+            onChange={(e) => setSendMessage(e.target.value)}
+            disabled={!connected || !subscribed}
+          />
+          <button
+            className={[
+              "bg-teal-600",
+              "text-white",
+              "font-bold",
+              "rounded-md",
+              "px-3",
+              "active:bg-teal-700/90",
+              "disabled:opacity-25",
+            ].join(" ")}
+            onClick={() => {
+              publishMqtt("test", sendMessage);
+              setSendMessage("");
+            }}
+            disabled={!connected || !subscribed || sendMessage === ""}
+          >
+            Send
+          </button>
+        </div>
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index} className={["my-4"].join(" ")}>
+              {message}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
---
+++ front/src/model/mqtt.ts
@@ -0,0 +1,23 @@
+import mqtt from "mqtt";
+
+const url = import.meta.env.VITE_MQTT_URL || `ws://localhost:8083/mqtt`;
+
+const mqtt_client = mqtt.connect(url);
+
+export const connectMqtt = (onConnect: () => void) => {
+  mqtt_client.on("connect", onConnect);
+};
+
+export const subscribeMqtt = (
+  topic: string,
+  onMessage: (topic: string, message: string) => void
+) => {
+  mqtt_client.subscribe(topic);
+  mqtt_client.on("message", (topic, message) => {
+    onMessage(topic, message.toString());
+  });
+};
+
+export const publishMqtt = (topic: string, message: string) => {
+  mqtt_client.publish(topic, message);
+};
