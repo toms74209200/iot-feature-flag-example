6db7d7e06a7f7c9d323ed9e356939f6b89ec1216
toms74209200 <motomotomato@gmail.com>
2025-01-16 19:00:29 +0900

    :sparkles: Update MessageCard

--- front/src/components/MessageCard.tsx
+++ front/src/components/MessageCard.tsx
@@ -5,17 +5,23 @@ interface MessageCardProps {
 }
 
 export const MessageCard = ({ message }: MessageCardProps) => {
+  const msg = (() => {
+    if ("name" in message) {
+      return message.name;
+    } else if ("message" in message) {
+      return message.message;
+    } else if ("timestamp" in message) {
+      return message.timestamp;
+    }
+    return null;
+  })();
   return (
     <div className={["p-4", "rounded-md"].join(" ")}>
       <p className={["text-sm", "text-gray-600"].join(" ")}>
         {message.clientId}
       </p>
       <div className={["flex", "flex-row", "justify-end"].join(" ")}>
-        {"name" in message ? (
-          <p>{message.name}</p>
-        ) : "message" in message ? (
-          <p>{message.message}</p>
-        ) : null}
+        {msg && <p>{msg}</p>}
       </div>
     </div>
   );
